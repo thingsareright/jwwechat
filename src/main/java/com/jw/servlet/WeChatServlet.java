91d138e1fecaa5d9d368b3d6a671878bafcb56d0
simpleJian <786274022@qq.com>
2018-04-27 19:45:06 +0800

    create msg bean

--- src/main/java/com/jw/servlet/WeChatServlet.java
+++ src/main/java/com/jw/servlet/WeChatServlet.java
@@ -1,14 +1,17 @@
 package com.jw.servlet;
 
+import com.jw.bean.WeChatMsgBean;
 import com.jw.bean.WeChatSecureBean;
 import com.jw.config.JwConfig;
 import com.jw.handler.impl.DefaultVerifyHandler;
+import com.jw.tools.PublicTools;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class WeChatServlet extends HttpServlet {
 
@@ -39,7 +42,25 @@ public class WeChatServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String> entityMap = PublicTools.getMapFromStream(req.getInputStream());
+        String msgType = entityMap.get("MsgType");
+        switch (msgType) {
+            case WeChatMsgBean.MSGTYPE_OF_TEXT:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_IMAGE:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_VOICE:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_VIDEO:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_SHORTVIDEO:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_LOCATION:
+                break;
+            case WeChatMsgBean.MSGTYPE_OF_LINK:
+                break;
 
+        }
     }
 
 }
--- src/main/java/com/jw/tools/PublicTools.java
+++ src/main/java/com/jw/tools/PublicTools.java
@@ -1,6 +1,16 @@
 package com.jw.tools;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class PublicTools {
 
@@ -17,4 +27,60 @@ public class PublicTools {
     public static String sha1Hex(String str) {
         return DigestUtils.sha1Hex(str);
     }
+
+
+    /**
+     * 从输入流中获取xml文档，并把xml文档转为map对象返回
+     *
+     * @return
+     */
+    public static Map<String, String> getMapFromStream(InputStream is) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        SAXReader reader = new SAXReader();
+        List<Element> elements = null;
+
+        try {
+            elements = reader.read(is).getRootElement().elements();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+            map.put("MsgType", null);
+            return map;
+        }
+
+        for (Element element : elements) {
+            map.put(element.getName(), element.getText());
+        }
+        return map;
+    }
+
+    /**
+     * 根据map获取传来的值信息
+     *
+     * @param instance
+     * @param map
+     * @param <T>
+     * @return
+     */
+    public static <T> T getEntityByMap(Class<T> instance, Map<String, String> map) {
+        T t = null;
+        try {
+            t = instance.newInstance();
+
+            Set<String> keys = map.keySet();
+
+            for (String key : keys) {
+                String value = map.get(key);
+                Method method = instance.getMethod("set" + key, String.class);
+                method.invoke(t, value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return t;
+    }
+
+
+
 }
